4e9090f708c64acb22b15d7974daf9caa72e2c3e
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-11 20:45:24 +0900

    AWS CloudFrontの使用

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,13 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: Rails.application.credentials.dig(:aws, :region)
-  }
-  config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
+if Rails.evn.producttion?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider:'AWS',
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region: Rails.application.credentials.dig(:aws, :region)
+    }
+    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
+    config.asset_host = "https://d1nqeym59hm5u0.cloudfront.net"
+
+  end
 end
